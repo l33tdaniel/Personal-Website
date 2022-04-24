4277906441d2956a902ba4e0f55690e4a17ab963
l33tDanelLocal <danielneugent@icloud.com>
2022-04-24 17:55:21 -0500

    made my own json + changed css

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
 
+// opening up a new app on port 3000
 app.listen(3000, () => {
   console.log("Application started and Listening on port 3000");
 });
 
+// the purpose of using static is so that the CSS will work - otherwise no CSS for us
 app.use(express.static(__dirname));
 
+// this is opening a path for requests and resolves for index.html
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
