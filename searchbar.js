b2fabf60221fb163650c9faf45f9875ba5d9cfa9
l33tDanelLocal <danielneugent@icloud.com>
2022-04-24 23:17:57 -0500

    json scanner now working

--- searchbar.js
+++ searchbar.js
@@ -1,15 +1,14 @@
 function search_movies() {
    
     let searchInput = document.getElementById('search-input').value;
- 
-    console.log(data.name);
+
     // this fetches the json
     fetch('movies.json')
         .then(data => data.json())
         .then(data => {
             // this takes the json and allows us to loop through for each movie
             for(let i = 0; i < Object.keys(data).length; i++) {
-                console.log(data[i]);
+                console.log(data[i].name);
 
             }
  
