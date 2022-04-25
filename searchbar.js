c0d22e95664f59305b70024e589d203b15365498
l33tDanelLocal <danielneugent@icloud.com>
2022-04-25 01:00:48 -0500

    added more search functionality

--- searchbar.js
+++ searchbar.js
@@ -2,6 +2,7 @@ function search_movies() {
     let searchInput = document.getElementById('search-input').value;
     let fixedInput = searchInput.toLowerCase();
     let display = document.getElementById('display');
+    let count = 0;
 
     let html = "";
     // this fetches the json
@@ -11,14 +12,16 @@ function search_movies() {
            // this makes it all lower case and tests to see if the movie displays.
             for(let i = 0; i < Object.keys(data).length; i++) {
                 if(data[i].name.toLowerCase().includes(fixedInput) && fixedInput != " ") {
-                    console.log("yes");
                     console.log(data[i].name);
-                    html += "<div> Name: " + data[i].name + 
-                            " Release date: " + data[i]["release date"];
+                    html += "<div id='movie'> Name: " + data[i].name + 
+                            " Release Date: " + data[i]["release date"] + "</div>";
                 }
                 else {
+                    count++;
                     console.log("nope");
-                    
+                    if(count >= Object.keys(data).length) {
+                        html += "<div> No movies are found by this title </div>";
+                    }
                 }
             }
             document.getElementById('display').innerHTML = html;
