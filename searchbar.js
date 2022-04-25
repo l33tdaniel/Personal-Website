5bbb75e79c617c011e600525ebfd6b74a761e7f5
l33tDanelLocal <danielneugent@icloud.com>
2022-04-25 00:21:41 -0500

    search functionality complete

--- searchbar.js
+++ searchbar.js
@@ -1,17 +1,22 @@
 function search_movies() {
-   
     let searchInput = document.getElementById('search-input').value;
+    let fixedInput = searchInput.toLowerCase();
 
+    let test = "";
+    let temp = "";
     // this fetches the json
     fetch('movies.json')
         .then(data => data.json())
         .then(data => {
-            // this takes the json and allows us to loop through for each movie
+           // this makes it all lower case and tests to see if the movie displays.
             for(let i = 0; i < Object.keys(data).length; i++) {
-                console.log(data[i].name);
-
+                if(data[i].name.toLowerCase().includes(fixedInput) && fixedInput != " ") {
+                    console.log("yes");
+                    return;
+                }
+                else {
+                    console.log("nope");
+                }
             }
- 
     });
-
 }
